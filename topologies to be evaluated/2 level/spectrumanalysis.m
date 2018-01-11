5758cc84c9dad68d764f0ad5c90c9523e4c8c9b4
MSI\hakan <hakansrc@msn.com>
2018-01-11 11:16:01 +0300

    bug fixes & additions, only the calculation of the efficiency is left, need to learn it

--- topologies to be evaluated/2 level/spectrumanalysis.m
+++ topologies to be evaluated/2 level/spectrumanalysis.m
@@ -3,29 +3,48 @@ cd('C:\Users\hakan\Documents\GitHub\mlv_inv\topologies to be evaluated\2 level')
 open_system('two_level_spwm.slx');
 %simOut = sim('two_level_spwm.slx','SimulationMode','normal','AbsTol','1e-6','SaveState','on','StateSaveName','xout','SaveOutput','on','OutputSaveName','yout','SaveFormat', 'Dataset');
 N = 2^20;
+
+%% values of the signals
 ma = 0.8;
-ref_frequency = 2*pi*50;
-sw_frequency = 2000;
-Sampling_time = 50e-6; %sampling frequency of the model
+ref_frequency = 2*pi*50; %radians per sec
+sw_frequency = 32250; %Hz
+Sampling_time = 1/(20*sw_frequency); %sampling frequency of the model
+Fs = 0.5/Sampling_time;  %Sampling Frequency for the spectrum analysis  %5e-6 goes up to 50kHz band
 stop_time = 0.5; %duration of the model
+set_param('two_level_spwm', 'StopTime', 'stop_time')  % duration of the simulation
+set_param('two_level_spwm/powergui','SampleTime','Sampling_time'); % sampling frequency of the simulation
+set_param('two_level_spwm/Subsystem/PhaseA_Ref','amplitude','ma','frequency','ref_frequency'); % setting ma and freq values of the ref sine waves
+set_param('two_level_spwm/Subsystem/PhaseB_Ref','amplitude','ma','frequency','ref_frequency'); % setting ma and freq values of the ref sine waves
+set_param('two_level_spwm/Subsystem/PhaseC_Ref','amplitude','ma','frequency','ref_frequency'); % setting ma and freq values of the ref sine waves
+get_param('two_level_spwm/','SimulationTime')
+%% Load&Source settings
+Load_Real_Power = 8500; %W
+Load_Inductive_Power = 4116; %VAr 
+DC_Voltage_Source = 540; %Volts
+Load_Nominal_Freq = 50; %Hz
+DCLINK_Cap1 = 100e-6; %Farads
+DCLINK_Cap2 = 100e-6; %Farads
 
-set_param('two_level_spwm', 'StopTime', 'stop_time')
-set_param('two_level_spwm/powergui','SampleTime','Sampling_time');
-set_param('two_level_spwm/Subsystem/PhaseA_Ref','amplitude','ma','frequency','ref_frequency');
-set_param('two_level_spwm/Subsystem/PhaseB_Ref','amplitude','ma','frequency','ref_frequency');
-set_param('two_level_spwm/Subsystem/PhaseC_Ref','amplitude','ma','frequency','ref_frequency');
+set_param('two_level_spwm/Load1','activePower','Load_Real_Power');
+set_param('two_level_spwm/Load1','activePower','Load_Inductive_Power');
+set_param('two_level_spwm/Load1','nominalfrequency','Load_Nominal_Freq');
+set_param('two_level_spwm/DC Voltage Source','amplitude','DC_Voltage_Source');
+set_param('two_level_spwm/DCLINK_Cap1','capacitance','DCLINK_Cap1')
+set_param('two_level_spwm/DCLINK_Cap2','capacitance','DCLINK_Cap2')
 
-set_param('two_level_spwm/Subsystem/Carrier_signal','freq','sw_frequency');
 
-simOut = sim('two_level_spwm.slx');
 
-figure
-plot(Phase_currents.time,[Phase_currents.signals(1).values]);
+set_param('two_level_spwm/Subsystem/Carrier_signal','freq','sw_frequency'); % setting freq value of the carrier
+
+simOut = sim('two_level_spwm.slx'); %run the simulation
+
+% figure
+% plot(Phase_currents.time,[Phase_currents.signals(1).values]);
 
 
 
 %% Spectrum of Ia
-Fs = numel(Phase_currents.signals(1).values);  %Sampling Frequency
+% Fs = numel(Phase_currents.signals(1).values);  %Sampling Frequency
 Ia_Spectrum = fft(Phase_currents.signals(1).values,N*2);
 Ia_Spectrum_abs = abs(Ia_Spectrum(2:N/2));
 freq = (1:N/2-1)*Fs/N;   
@@ -43,7 +62,7 @@ ylabel('Magnitude');
 % xlabel('Frequency (Hz)');
 % % axis([0 4000 10^-4 1])                
 %% Spectrum of DCLINK_current
-Fs = numel(DCLINK_current.data);  %Sampling Frequency
+% Fs = numel(DCLINK_current.data);  %Sampling Frequency
 DCLINK_current_spectrum = fft(DCLINK_current.data,N*2);
 DCLINK_current_spectrum_abs = abs(DCLINK_current_spectrum(2:N/2));
 freq = (1:N/2-1)*Fs/N;   
@@ -55,7 +74,7 @@ title('Spectrum of DCLINK_current');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
 %% Spectrum of DCLINK_voltage
-Fs = numel(DCLINK_voltage.data);  %Sampling Frequency
+% Fs = numel(DCLINK_voltage.data);  %Sampling Frequency
 DCLINK_voltage_spectrum = fft(DCLINK_voltage.data,N*2);
 DCLINK_voltage_spectrum_abs = abs(DCLINK_voltage_spectrum(2:N/2));
 freq = (1:N/2-1)*Fs/N;   
@@ -67,7 +86,7 @@ title('Spectrum of DCLINK_voltage');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
 %% Spectrum of VAB only
-Fs = numel(LL_voltages.signals(1).values);  %Sampling Frequency
+% Fs = numel(LL_voltages.signals(1).values);  %Sampling Frequency
 LL_voltages_spectrum = fft(LL_voltages.signals(1).values,N*2);
 LL_voltages_spectrum_abs = abs(LL_voltages_spectrum(2:N/2));
 freq = (1:N/2-1)*Fs/N;   
@@ -82,9 +101,41 @@ ylabel('Magnitude');
 
 close_system('two_level_spwm.slx',false);
 
+%% Spectrum of VAN 
+% Fs = numel(LN_voltages.signals(1).values);  %Sampling Frequency
+LN_voltages_spectrum = fft(LN_voltages.signals(1).values,N*2);
+LN_voltages_spectrum_abs = abs(LN_voltages_spectrum(2:N/2));
+freq = (1:N/2-1)*Fs/N;   
 
+LN_voltages_spectrum_abs = LN_voltages_spectrum_abs/max(LN_voltages_spectrum_abs); % normalization
+figure;
+semilogy(freq,LN_voltages_spectrum_abs) % Plot the magnitude of the samples of CTFT of the audio signal
+title('Spectrum of Van');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
 
+%% plotting of THD's
+figure
+subplot(3,1,1);
+plot(THD_Ia.time, 100*THD_Ia.data)
+title('THD of Ia');
+xlabel('Time(sec)');
+ylabel('THD (%)');
+close_system('two_level_spwm.slx',false);
 
+subplot(3,1,2);
+plot(THD_Van.time, 100*THD_Van.data)
+title('THD of Van');
+xlabel('Time(sec)');
+ylabel('THD (%)');
+
+subplot(3,1,3);
+plot(THD_Vab.time, 100*THD_Vab.data)
+title('THD of Vab');
+xlabel('Time(sec)');
+ylabel('THD (%)');
+
+close_system('two_level_spwm.slx',false);
 
 
 
