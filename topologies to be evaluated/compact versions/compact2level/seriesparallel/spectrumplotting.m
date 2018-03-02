2a26ec5d4c0873bfbf23c8bcb6cc440ab42d5702
MSI\hakan <hakansrc@msn.com>
2018-03-02 14:48:23 +0300

    i guess i have handled the situation with the plotting of the spectrums, gonna start to collect datas soon

--- topologies to be evaluated/compact versions/compact2level/seriesparallel/spectrumplotting.m
+++ topologies to be evaluated/compact versions/compact2level/seriesparallel/spectrumplotting.m
@@ -18,6 +18,7 @@ Fs = 1/Sampling_time;  %Sampling Frequency for the spectrum analysis  %5e-6 goes
 % % % % % % % % % % % P = power_fftscope(Phase_currents1);
 % % % % % % % % % % % P.set('MaxFrequency','sw_frequency')
 
+
 N = numel(twolevelseriesparallel_interleaved.get('LL_voltages1').signals(1).values);
 % power_fftscope(Phase_currents1);
 Fs = 1/Sampling_time;
@@ -25,11 +26,34 @@ LL_voltages_spectrum = fft(twolevelseriesparallel_interleaved.get('LL_voltages1'
 LL_voltages_spectrum_abs = abs(LL_voltages_spectrum(2:N/2));
 freq = (1:N/2-1)*Fs/N;       
 figure;
-plot(freq,LL_voltages_spectrum_abs)% Plot the magnitude of the samples of CTFT of the audio signal
+semilogy(freq,LL_voltages_spectrum_abs)% Plot the magnitude of the samples of CTFT of the audio signal
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
 
+% timelength = round((numel(twolevelseriesparallel_interleaved.get('DCLINK1_voltage').time))*0.9);
+% N = numel(twolevelseriesparallel_interleaved.get('DCLINK1_voltage').signals.values);
+% % power_fftscope(Phase_currents1);
+% Fs = 1/Sampling_time;
+% DCLINK_voltage_spectrum = fft(twolevelseriesparallel_interleaved.get('DCLINK1_voltage').signals.values,N)/(0.5*N);
+% DCLINK_voltage_spectrum_abs = abs(DCLINK_voltage_spectrum(2:N/2));
+% freq = (1:N/2-1)*Fs/N;       
+% figure;
+% semilogy(freq,DCLINK_voltage_spectrum_abs)% Plot the magnitude of the samples of CTFT of the audio signal
+% xlabel('Frequency (Hz)');
+% ylabel('Magnitude');
 
+
+N = numel(twolevelseriesparallel_interleaved.get('DCLINK1_voltage').signals.values);
+% power_fftscope(Phase_currents1);
+Fs = 1/Sampling_time;
+DCLINK_voltage_spectrum = fft(twolevelseriesparallel_interleaved.get('DCLINK1_voltage').signals.values,N)/(0.5*N);
+DCLINK_voltage_spectrum_abs = abs(DCLINK_voltage_spectrum(1:(N/2+1)));
+DCLINK_voltage_spectrum_abs(1) = DCLINK_voltage_spectrum_abs(1)/2;
+freq = (0:(N/2))*Fs/N;       
+figure;
+semilogy(freq,DCLINK_voltage_spectrum_abs)% Plot the magnitude of the samples of CTFT of the audio signal
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
 
 
 
